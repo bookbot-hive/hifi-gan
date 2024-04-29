6c625630ea31577e65a97af484e186e9bf2f3959
w11wo <wilsonwong961@gmail.com>
2024-04-29 06:01:39 +0000

    Added fine-tuning Script

--- run_id.sh
+++ run_id.sh
@@ -1,9 +1,22 @@
-python train.py \
+# 929 data / 16 bs = 58 steps/ep
+# 2.4M steps / 58 steps/ep = 41379 eps
+# 200k steps / 58 steps/ep = 3500 eps
+
+# python train.py \
+#     --config config_v4.json \
+#     --input_wavs_dir /root/id-ID-Althaf \
+#     --input_training_file ID/training.txt \
+#     --input_validation_file ID/validation.txt \
+#     --training_epoch 41400 \
+#     --checkpoint_interval 100000 \
+#     --validation_interval 10000
+
+python fine_tune.py \
     --config config_v4.json \
     --input_wavs_dir /root/id-ID-Althaf \
     --input_training_file ID/training.txt \
     --input_validation_file ID/validation.txt \
-    --training_epoch 41400 \
+    --training_epoch 3500 \
     --checkpoint_interval 100000 \
     --validation_interval 10000 \
-    --fp16 true --torch_dtype float16
+    --fp16 true --torch_dtype bfloat16
